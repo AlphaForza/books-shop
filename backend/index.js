145899af2f48cb1dd7216b40330d6a03f260d206
AlphaDev <30984729+AlphaForza@users.noreply.github.com>
2023-01-18 00:55:16 +0100

    insert into base from backend

--- backend/index.js
+++ backend/index.js
@@ -10,6 +10,8 @@ const db = mysql.createConnection({
 	database: 'bookshop',
 });
 
+app.use(express.json());
+
 app.get('/', (req, res) => {
 	res.json('this is the backend');
 });
@@ -17,6 +19,16 @@ app.get('/', (req, res) => {
 app.get('/books', (req, res) => {
 	const q = 'SELECT * FROM books';
 	db.query(q, (err, data) => {
+		if (err) return res.json(err);
+		return res.json(data);
+	});
+});
+
+app.post('/books', (req, res) => {
+	const q = 'INSERT INTO books (`title`,`desc`, `cover`) VALUES (?)';
+	const values = [req.body.title, req.body.desc, req.body.cover];
+
+	db.query(q, [values], (err, data) => {
 		if (err) return res.json(err);
 		return res.json(data);
 	});
